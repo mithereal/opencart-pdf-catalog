8741bd7e0903d2847e726dc106a6a2b52e3deded
Ramiro Aparicio <rapariciog@gmail.com>
2014-05-25 17:12:32 +0200

    Translate backend to spanish

---
+++ upload/admin/language/spanish/module/pdf_catalog.php
@@ -0,0 +1,44 @@
+<?php
+// Heading
+$_['heading_title']    = 'Cat&aacute;logo en PDF ';
+
+// Text
+$_['text_module']      = 'M&oacute;dulos';
+$_['text_success']     = '¡Exito: Has cambiado la configuraci&oacuten del m&oacutedulo!';
+$_['text_hide']       = 'Ocultar';
+$_['text_description']       = 'Descripci&oacute;n';
+$_['text_content_top']    = 'Contenido superior';
+$_['text_content_bottom'] = 'Contenido inferior';
+$_['text_column_left']    = 'Columna izquierda';
+$_['text_column_right']   = 'Columna derecha';
+$_['text_yes'] = 'Si';
+$_['text_no'] = 'No';
+$_['text_native'] = 'Nativo';
+
+// Entry
+$_['entry_layout']        = 'Formato:';
+$_['entry_position']   = 'Posici&oacute;n:';
+$_['entry_status']     = 'Estado:';
+$_['entry_display_toc'] = 'Mostrar &Iacute;ndice:';
+$_['entry_sort_order'] = 'Orden:';
+$_['entry_description'] = 'Introducci&oacute;n en el PDF:';
+$_['entry_display_categories'] = 'Mostrar categor&iacute;as:';
+$_['entry_pdf_creator'] = 'Creador:';
+$_['entry_pdf_author'] = 'Autor:';
+$_['entry_pdf_title'] = 'T&iacute;tulo:';
+$_['entry_pdf_max_products'] = 'L&iacute;mite de Productos:';
+$_['entry_pdf_subject'] = 'Asunto:';
+$_['entry_pdf_keywords'] = 'Keywords:';
+$_['entry_pdf_catalog_display_description'] = 'Mostrar descripci&oacute;n:';
+$_['entry_pdf_catalog_image_height'] = 'Altura de Imagen:';
+$_['entry_pdf_catalog_image_width'] = 'Ancho de Imagen:';
+$_['entry_pdf_catalog_item_per_page'] = 'Limite de productos por p&aacute;gina:';
+$_['entry_pdf_catalog_description_chars'] = 'Limite de caracteres en descripci&oacute;n:';
+$_['entry_pdf_catalog_template_type'] = 'Tipo de plantilla:';
+$_['entry_display_out_of_stock'] = 'Mostrar productos sin stock:';
+$_['entry_display_disabled'] = 'Mostrar products deshabilitados:';
+$_['entry_display_subcategories'] = 'Mostrar subcategor&iacute;as:';
+
+// Error
+$_['error_permission'] = '¡Error: No tienes permisos para cambiar la configuraci&oacute;n!';
+?>
