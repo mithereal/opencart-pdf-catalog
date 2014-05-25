8d44e04ced6aa634995dfaa97e7a0d29c4c8a936
Ramiro Aparicio <rapariciog@gmail.com>
2014-05-25 17:00:44 +0200

    Add catalog spanish translation
    Add spanish translation to the user views

---
+++ upload/catalog/language/spanish/module/pdf_catalog.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title'] = 'Catalogo en PDF';
+
+$_['text_select']	= '-- Selecciona --';
+$_['text_all_categories']	= 'Todas las categor&iacute;as';
+// PDF
+$_['text_category'] = 'Producto:';
+$_['text_model'] = 'Modelo:';
+$_['text_price'] = 'Precio:';
+$_['text_description']       = 'Descripci&oacute;n:';
+$_['text_category_title'] = 'CATEGOR&Iactue;AS';
+
+?>
